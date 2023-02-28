233e2696dcfdcb5b156b594653c59f7922851bdf
myke <myke.chastang@orange.fr>
2023-02-28 12:39:24 +0100

    Mise en place contexte pour utilisation des clefs

--- myfit-back/src/EventListener/JWTListener.php
+++ myfit-back/src/EventListener/JWTListener.php
@@ -9,17 +9,24 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class JWTListener implements EventSubscriberInterface {
 
+    private $SECRETKEY;
+
+    public function __construct($secrekey)
+    {
+        $this->SECRETKEY = $secrekey;
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
-        $jwt = $event->getRequest()->headers->get('jwt-x-token');
-
-        $jwtAllPieces = preg_split('.', $jwt);
-
-        $jwtReconstitution =  $jwtAllPieces[0] + $jwtAllPieces[1];
-
-        if ($jwtAllPieces[2] != $jwtReconstitution) {
-            $event->setResponse(new JsonResponse(['message' => 'NON']));
-        }
+//        $jwt = $event->getRequest()->headers->get('jwt-x-token');
+//
+//        $jwtAllPieces = preg_split('.', $jwt);
+//
+//        $jwtReconstitution =  $jwtAllPieces[0] + $jwtAllPieces[1];
+//
+//        if ($jwtAllPieces[2] != $jwtReconstitution) {
+//            $event->setResponse(new JsonResponse(['message' => 'NON']));
+//        }
     }
 
     public static function getSubscribedEvents(): array
