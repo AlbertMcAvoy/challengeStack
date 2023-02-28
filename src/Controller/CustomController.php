cb18a75e9b8f473c33613933db50678b46171188
arobbrechtadrenalead <40170910+IGSparkew@users.noreply.github.com>
2023-02-28 09:58:22 +0100

    test

---
+++ src/Controller/CustomController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\I_TokenAuthenticationController;
+
+class CustomController extends AbstractController implements I_TokenAuthenticationController {
+
+}
+
+
+?>
