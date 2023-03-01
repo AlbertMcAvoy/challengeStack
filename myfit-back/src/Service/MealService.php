a4a0f709018d7d4f560fe2b92f3a7322a2f421e9
arobbrechtadrenalead <40170910+IGSparkew@users.noreply.github.com>
2023-03-01 12:54:09 +0100

    finish Meal Service

--- myfit-back/src/Service/MealService.php
+++ myfit-back/src/Service/MealService.php
@@ -29,7 +29,31 @@ class MealService
     public function getAllMealByUser() {
         $user = $this->userService->getCurrentUser();
         if ($user == null) return [];
-        return $this->mealRepository->findBy(['user' => $user]);
+        $meals = $this->mealRepository->findBy(['user' => $user]);
+        $toReturn = [];
+
+        foreach($meals as $meal) {
+            $foods =  $meal->getFood()->toArray();
+            $foodsDTO = [];
+            foreach($foods as $food) {
+                array_push($foodsDTO, [
+                    'id'=>$food->getId(),
+                    'libelle'=>$food->getLibelle(),
+                    'calories' => $food->getCalories()
+                ]);
+            }
+    
+            array_push($toReturn , [
+                'id' => $meal->getId(),
+                'name' => $meal->getName(),
+                'date' => $meal->getDateTime(),
+                'foods' => $foodsDTO
+            ]);
+    
+        }
+
+        return $toReturn;
+       
     }
 
     public function getMeal(DateTime $date = null)
@@ -56,11 +80,21 @@ class MealService
         if (empty($id)) return [];
         $user = $this->userService->getCurrentUser();
         $meal = $this->mealRepository->findOneBy(['id' => $id, 'user' => $user]);
+        $foods =  $meal->getFood()->toArray();
+        $foodsDTO = [];
+        foreach($foods as $food) {
+            array_push($foodsDTO, [
+                'id'=>$food->getId(),
+                'libelle'=>$food->getLibelle(),
+                'calories' => $food->getCalories()
+            ]);
+        }
+
         $toReturn = [
             'id' => $meal->getId(),
             'name' => $meal->getName(),
             'date' => $meal->getDateTime(),
-            'foods' => $meal->getFood()->toArray()
+            'foods' => $foodsDTO
         ];
 
         return $toReturn;
