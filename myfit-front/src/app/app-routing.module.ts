ff34af04c79fa3eb68c58cf0e8dcb6bbf6b5c58c
Amine Fajry <amine.f@dalvin.fr>
2023-03-01 18:48:56 +0100

    feat: add error page

--- myfit-front/src/app/app-routing.module.ts
+++ myfit-front/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from "./module/LoginPageComponent/loginPage-compon
 import { HomePageComponent } from "./module/HomePageComponent/homePage-component";
 import { RegisterPageComponent } from "./module/RegisterPageComponent/registerPage-component";
 import {MonCompteComponent} from "./module/monComptePageComponent/monCompte.component";
+import {ErrorPageComponent} from "./module/ErrorPageComponent/errorPage-component";
 
 const routes: Routes = [
   {
@@ -25,6 +26,10 @@ const routes: Routes = [
   {
     path: 'compte',
     component: MonCompteComponent,
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
   }
 ];
 
