e51e4f6614d64426ddaee951be05764805e306a7
arobbrechtadrenalead <40170910+IGSparkew@users.noreply.github.com>
2023-03-01 10:55:41 +0100

    create getCurrentUser and add crud

--- myfit-back/src/Controller/BodyController.php
+++ myfit-back/src/Controller/BodyController.php
@@ -3,69 +3,66 @@
 namespace App\Controller;
 
 use App\Entity\Body;
-use App\Form\BodyType;
 use App\Repository\BodyRepository;
+use App\Service\UserService;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use function PHPUnit\Framework\isEmpty;
 
 class BodyController extends AbstractController
 {
-    public function index(BodyRepository $bodyRepository): Response
+    public function index(UserService $userService,BodyRepository $bodyRepository): JsonResponse
     {
-        return $this->render('body/index.html.twig', [
-            'bodies' => $bodyRepository->findAll(),
-        ]);
+
+        $user = $userService->getCurrentUser();
+        if ($user == null) return $this->json(["status" => 404, "message" => "User not found with this token !"]);
+        $bodies = $bodyRepository->findBy(['user' => $user]);
+        $jsonToReturn = [];
+        foreach($bodies as $body) {
+            array_push($jsonToReturn, [
+            "weight" => $body->getWeight(), 
+            "objectif_weight" => $body->getObjectifWeight(), 
+            "date" => $body->getDateTime()
+            ]);
+        }
+
+        return $this->json($jsonToReturn);
     }
 
-    public function new(Request $request, BodyRepository $bodyRepository): Response
+    public function new(UserService $userService, Request $request, BodyRepository $bodyRepository): JsonResponse
     {
         $body = new Body();
-        $form = $this->createForm(BodyType::class, $body);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $bodyRepository->save($body, true);
-
-            return $this->redirectToRoute('app_body_index', [], Response::HTTP_SEE_OTHER);
+        $user = $userService->getCurrentUser();
+        if ($user == null) return $this->json(["status" => 404, "message" => "User not found with this token !"]);
+        $data = json_decode($request->getContent(), true);
+        if (!empty($data["objectif_weight"] && !empty($data["weight"]))) {
+            $body->setWeight($data["weight"])
+            ->setObjectifWeight($data["objectif_weight"])
+            ->setDateTime(new DateTime());
+            
+            $bodyRepository->save($body);
+            return $this->json(["status" => 200, "message" => "The Body is created"]);
         }
-
-        return $this->renderForm('body/new.html.twig', [
-            'body' => $body,
-            'form' => $form,
-        ]);
+        
+        return $this->json(["status" => 400, "message" => "Error when the data is enter"]);
     }
 
-    public function show(Body $body): Response
+
+    public function delete(UserService $userService, Request $request, BodyRepository $bodyRepository): Response
     {
-        return $this->render('body/show.html.twig', [
-            'body' => $body,
-        ]);
-    }
+        $user = $userService->getCurrentUser();
+        if ($user == null) return $this->json(["status" => 404, "message" => "User not found with this token !"]);
+        $id = $request->attributes->get('id');
 
-    public function edit(Request $request, Body $body, BodyRepository $bodyRepository): Response
-    {
-        $form = $this->createForm(BodyType::class, $body);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $bodyRepository->save($body, true);
-
-            return $this->redirectToRoute('app_body_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('body/edit.html.twig', [
-            'body' => $body,
-            'form' => $form,
-        ]);
-    }
-
-    public function delete(Request $request, Body $body, BodyRepository $bodyRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$body->getId(), $request->request->get('_token'))) {
+        $body = $bodyRepository->findOneBy(['id' => $id, 'user' => $user]);
+        if ($body) {
             $bodyRepository->remove($body, true);
+            return $this->json(["status" => 200, "message" => "Body is deleted"]);
         }
-
-        return $this->redirectToRoute('app_body_index', [], Response::HTTP_SEE_OTHER);
+        return $this->json(["status" => 400, "message" => "Body not found"]);
     }
 }
--- myfit-back/src/Service/UserService.php
+++ myfit-back/src/Service/UserService.php
@@ -1,6 +1,7 @@
 <?php 
 namespace App\Service;
 
+use App\Entity\Body;
 use App\Entity\User;
 use DateTime;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -8,19 +9,35 @@ use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UserRepository;
 use App\Exception\UserExistException;
 use App\Exception\UserFieldFromException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class UserService {
 
     private $userPasswordHasher;
     private $entityManager;
     private $userRepository;
+    private $tokenStorage;
 
 
-    public function __construct(UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserRepository $userRepository) {
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserRepository $userRepository, TokenStorageInterface $tokenStorage) {
         $this->userPasswordHasher = $userPasswordHasher;
         $this->entityManager = $entityManager;
         $this->userRepository = $userRepository;
+        $this->tokenStorage = $tokenStorage;
     }
+
+    public function getCurrentUser(): User {
+        $token = $this->tokenStorage->getToken();
+
+        if ($token instanceof TokenInterface) {
+            $user = $token->getUser();
+            return $user;
+        }
+
+        return null;
+    }
+
 
     public function register(Array $data) {
         $user = new User();
@@ -29,7 +46,7 @@ class UserService {
         if (!empty($data['firstname']) && !empty($data['lastname']) && !empty($data['email']) && !empty($data['password']) && !empty($data['height'])) {
 
             $userFind = $this->userRepository->findBy(["email"=>$data["email"]]);
-
+            
             if (empty($userFind)) {
                 $user->setFirstName($data['firstname'])
                 ->setLastName($data['lastname'])
@@ -40,6 +57,14 @@ class UserService {
                 ->setAge($data['age'] ?? null)
                 ->setSubscriptionDate(new DateTime());
                 $user->setPassword($this->userPasswordHasher->hashPassword($user, $data['password']));
+
+                if (!empty($data['weight']) && !empty($data['objectif_weight'])) {
+                    $body = new Body();
+                    $body->setWeight($data['weight'])
+                    ->setObjectifWeight($data['objectif_weight'])
+                    ->setDateTime(new DateTime());
+                    $user->addBody($body);
+                } 
 
 
                 $this->entityManager->persist($user);
