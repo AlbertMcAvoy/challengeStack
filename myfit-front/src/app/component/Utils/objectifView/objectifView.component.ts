be54b47dfc66f6c0726c304903f152e78277544b
myke <myke.chastang@orange.fr>
2023-03-02 13:16:14 +0100

    [ADD] - Début récupération info user dans dashboard

--- myfit-front/src/app/component/Utils/objectifView/objectifView.component.ts
+++ myfit-front/src/app/component/Utils/objectifView/objectifView.component.ts
@@ -1,4 +1,5 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {User} from "../../../class/User";
 
 @Component({
   selector: 'objectif-view',
@@ -7,4 +8,5 @@ import {Component} from "@angular/core";
 })
 export class ObjectifViewComponent {
 
+  @Input() user: User | undefined;
 }
